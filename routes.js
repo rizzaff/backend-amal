b9b269de50d54119682ceeee06245d52d1090fe4
Haji Dito Murya Alfarohmi <hajidito.alfarohmi@pegadaian.co.id>
2020-03-14 11:22:08 +0700

    add pembayaran

--- routes.js
+++ routes.js
@@ -45,6 +45,7 @@ module.exports = function(app) {
 
     app.post('/gcash/getbalance', pembayaran.getGcashBalance);
     app.post('/gcash/create', pembayaran.PembentukanNomorGcash);
+    app.post('/gcash/transaksi', pembayaran.pembayaranGcash);
 
     app.get('/kendaraan/merk/:kategori',kendaraan.viewMerk);
     app.get('/kendaraan/merk/cari/:kategori/:cari',kendaraan.cariMerk);
